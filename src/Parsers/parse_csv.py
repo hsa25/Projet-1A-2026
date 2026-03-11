599c5e23b9599e16c32ec4fd5a01dbb2162d8953
ms2005 <mahmoud.serrhini@eleve.ensai.fr>
2026-03-11 11:35:05 +0000

    read csv

--- src/Parsers/parse_csv.py
+++ src/Parsers/parse_csv.py
@@ -1,10 +1,12 @@
 from src.Common.utils import print_timings
+import csv
 
 
 @print_timings
 def parse_players_csv(filepath: str, sep: str = ";") -> list:
-    raise Exception(
-        "Oh non, la méthode parce_csv n'a pas été implémentée, "
-        "vous allez devoir le faire vous-mêmes :("
-    )
-    return list()
+    liste = []
+    with open(filepath, 'r') as file:
+        reader = csv.reader(file, delimiter=sep)
+        for row in reader:
+            liste.append(row)
+    return liste
