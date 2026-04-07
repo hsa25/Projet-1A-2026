1888041073e73709f2228a02ac1eb0754204d7d7
hsa <hazem.soni-alami@eleve.ensai.fr>
2026-04-07 08:44:45 +0000

    class compet

---
+++ src/Model/Competition.py
@@ -0,0 +1,28 @@
+import datetime as dt
+from .Equipe import Equipe
+
+class Competition:
+
+    def __init__(self, nom: str,
+            date_debut: dt.date, 
+            date_fin: dt.date, 
+            participants: dict[str: "Equipe"]| None = None, 
+            sport: str,
+            classement: dict["Equipe": int]| None = None):
+            self.__nom= nom
+            self.__date_debut= date_debut
+            self.__date_fin= date_fin
+            self.__participants= participants
+            self.__sport= sport
+            self.__classement= classement 
+        
+    def ajouter_participant(self, Equipe_1):
+        if not isinstance(Equipe_1, Equipe):
+            raise TypeError("L'équipe doit être une équipe")
+        self.__participants[__str__(Equipe_1)]=Equipe_1
+
+    def retirer_participant(self, Equipe_1):
+        if not isinstance(Equipe_1, Equipe):
+            raise TypeError("L'équipe doit être une équipe")
+        del self.__participants[str(equipe_1)]        
+
