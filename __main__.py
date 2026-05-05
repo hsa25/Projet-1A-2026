24beefe5ba8f6dd46bb9fc6eb334e82aa850df94
hsa <hazem.soni-alami@eleve.ensai.fr>
2026-05-05 09:42:24 +0000

    proposer compet

--- __main__.py
+++ __main__.py
@@ -3,7 +3,8 @@ from src.Analysis import display all competitions
 
 supported_sport = [
     Sport(name="football", team_sport=True),
-    Sport(name="tennis",team_sport=False),
+    Sport(name="tennis atp",team_sport=False),
+    Sport(name="tennis wta",team_sport=False),
     Sport(name="volleyball", team_sport= True),
     Sport(name="basketball", team_sport=True),
     Sport(name="league of legends", team_sport=True),
@@ -18,49 +19,43 @@ if selected_sport not in supported_sports:
     Raise Exception("Le sport sélectionné n'est pas encore supporté par l'appli")
 
 
-display_all_competitions = {"tennis": ["atp", "wta"],
-                            "football": ["ligue des champions", "Jupiler League", 
+display_all_competitions = {"tennis atp": ["Brisbane", "United Cup","Hong Kong",
+                                        "Adelaide", "Auckland", "Australian Open",
+                                        "Montpellier", "Cordoba","Dallas", "Marseille",
+                                        "Delray Beach", "Buenos Aires", "Rotterdam",
+                                        "Doha", "Los Cabos", "Rio De Janeiro",
+                                        "Acapulco", "Dubai", "Santiago",
+                                        "Indian Wells", "Miami Masters", "Estoril",
+                                        "Houston", "Marrakech", "Monte Carlo Masters",
+                                        "Barcelona", "Munich", "Bucharest",
+                                        "Madrid Masters", "Rome Masters","Geneva",
+                                        "Lyon", "Roland Garros", "s Hertogenbosch",
+                                        "Stuttgart", "Halle", "Queen's Club",
+                                        "Mallorca","Eastbourne", "Wimbledon",
+                                        "Gstaad", "Hamburg", "Bastad", "Newport",
+                                        "Atlanta", "Kitzbuhel","Umag", "Washington",
+                                        "Paris Olympics","Canada Masters", "Cincinnati Masters",
+                                        "Winston-Salem", "Us Open", "Chengdu", "Hangzhou",
+                                        "Laver Cup", "Tokyo","Beijing", "Shanghai Masters",
+                                        "Almaty", "Antwerp", "Stockholm", "Basel",
+                                        "Vienna", "Paris Masters", "Belgrade",
+                                        "Metz", "Tour Finals", "Next Gen Finals",
+                                        "Davis Cup"],
+                            "football": ["Ligue des champions", "Jupiler League", 
                                         "Premier League", "Ligue 1", "Bundesliga", 
                                         "Serie A", "Eredivisie", "Ekstraklasa", 
                                         "Liga ZON Sagres","Scotland Premier League", 
                                         "LIGA BBVA", "Super League"],
-                            "badminton": ["Hommes", "Femmes"],
-                            "basketball": ["regular", "play-off"]}
+                            "badminton": ["JO 2024 Homme", "JO 2024 Femme"],
+                            "basketball": ["regular", "play-off"],
+                            "échecs":["Coupe du Monde d'échecs de la FIDE"],
+                            "counter strike 2": ["Major d'hiver 2025"],
+                            "league of legends":["LEC Winter Split 2025"],
+                            "starcraft 2": ["Global StarCraft II League 2016"],
+                            }
 selected_competitions = input(
     "Sélectionnez une compétition parmi {display_all_competitions(selected_sport)}")
     
-
-
-
-
-
-
-
-
-
-
-
-import pandas as pd
-
-from src.Parsers.parse_csv import parse_players_csv
-from src.Analysis.pandas.GoatFinder import find_the_goat_in_df
-from src.Analysis.homemade.GoatFinder import find_the_goat
-
-setting = input("Select a setting, 0=pandas-powered, 1=àlamain-powered\n")
-
-if setting == "0":
-    players_df = pd.read_csv("./data/players.csv")
-else:
-    players = parse_players_csv("./data/players.csv")
-
-print("Select your journey through the data")
-print("1 - I want to know who is the greatest football player of all time")
-input("2 - Just kidding, there's only one option\n")
-
-if setting == "0":
-    the_goat = find_the_goat_in_df(players_df)
-else:
-    the_goat = find_the_goat(players)
 
 
 
